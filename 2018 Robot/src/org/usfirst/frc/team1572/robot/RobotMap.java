e7803df9e742d5e82cc1f46b08516d6061bd4ae3
ethanw1 <sd.ethanw@gmail.com>
2018-01-10 08:03:33 -0800

    Added forklift subsystem and made further preparations for drivetrain

--- 2018 Robot/src/org/usfirst/frc/team1572/robot/RobotMap.java
+++ 2018 Robot/src/org/usfirst/frc/team1572/robot/RobotMap.java
@@ -10,8 +10,9 @@ package org.usfirst.frc.team1572.robot;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
@@ -25,13 +26,39 @@ public class RobotMap {
 	public static TalonSRX leftDriveSlave;
 	public static TalonSRX rightDriveMaster;
 	public static TalonSRX rightDriveSlave;
+	public static TalonSRX bottomForklift;
+	public static TalonSRX topForklift;
+	public double bottomLowLimit = 0;
+	public double bottomHighLimit = 0;
+	public double topLowLimit = 0;
+	public double topHighLimit = 0;
+	//Victors for practice robot
+	public static Victor leftDrive1;
+	public static Victor leftDrive2;
+	public static Victor rightDrive1;
+	public static Victor rightDrive2;
+	public static Encoder leftEncoder;
+	public static Encoder rightEncoder;
 	
 	public static void init() {
+		leftDriveMaster = new TalonSRX(1);
+		leftDriveSlave = new TalonSRX(2);
+		rightDriveMaster = new TalonSRX(3);
+		rightDriveSlave = new TalonSRX(4);
+		bottomForklift = new TalonSRX(5);
+		topForklift = new TalonSRX(6);
+		leftDrive1 = new Victor(4);
+		leftDrive2 = new Victor(5);
+		rightDrive1 = new Victor(6);
+		rightDrive2 = new Victor(7);
 		leftDriveMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
 		leftDriveSlave.follow(leftDriveMaster);
 		rightDriveMaster.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
 		rightDriveSlave.follow(rightDriveMaster);
-		//add PIDF configurations for speed control
+		//add PIDF configurations for drivetrain speed control
+		//add setup for encoders
+		bottomForklift.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
+		topForklift.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, 0, 0);
 	}
 	
 	// For example to map the left and right motors, you could define the
--- 2018 Robot/src/org/usfirst/frc/team1572/robot/subsystems/Drivetrain.java
+++ 2018 Robot/src/org/usfirst/frc/team1572/robot/subsystems/Drivetrain.java
@@ -5,6 +5,7 @@ import org.usfirst.frc.team1572.robot.RobotMap;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -14,6 +15,10 @@ public class Drivetrain extends Subsystem {
 	
 	TalonSRX leftMaster = RobotMap.leftDriveMaster;
 	TalonSRX rightMaster = RobotMap.rightDriveMaster;
+	Victor leftDrive1 = RobotMap.leftDrive1;
+	Victor leftDrive2 = RobotMap.leftDrive2;
+	Victor rightDrive1 = RobotMap.rightDrive1;
+	Victor rightDrive2 = RobotMap.rightDrive2;
 	
 	public void arcadeDriveVoltage(double x, double y, double maxX, double maxY) {
 		x *= maxX;
@@ -30,6 +35,12 @@ public class Drivetrain extends Subsystem {
 		}
 		leftMaster.set(ControlMode.PercentOutput, left);
 		rightMaster.set(ControlMode.PercentOutput, right);
+		System.out.println("leftMaster output percent " + leftMaster.getMotorOutputPercent());
+		System.out.println("rightMaster output percent " + rightMaster.getMotorOutputPercent());
+		leftDrive1.set(leftMaster.getMotorOutputPercent());
+		leftDrive2.set(leftMaster.getMotorOutputPercent());
+		rightDrive1.set(rightMaster.getMotorOutputPercent());
+		rightDrive2.set(rightMaster.getMotorOutputPercent());
 	}
 	public void arcadeDriveRPM(double x, double y, double maxX, double maxY, double maxRPM) {
 		x *= maxX;
@@ -48,6 +59,34 @@ public class Drivetrain extends Subsystem {
 		right *= maxRPM;
 		leftMaster.set(ControlMode.Velocity, left);
 		rightMaster.set(ControlMode.Velocity, right);
+		//make a rpm drive for Victors
+		/*System.out.println("leftMaster output percent " + leftMaster.getMotorOutputPercent());
+		System.out.println("rightMaster output percent " + rightMaster.getMotorOutputPercent());
+		leftDrive1.set(leftMaster.getMotorOutputPercent());
+		leftDrive2.set(leftMaster.getMotorOutputPercent());
+		rightDrive1.set(rightMaster.getMotorOutputPercent());
+		rightDrive2.set(rightMaster.getMotorOutputPercent());*/
+	}
+	public void individualMotorDriveVoltage(double left, double right) {
+		leftMaster.set(ControlMode.PercentOutput, left);
+		rightMaster.set(ControlMode.PercentOutput, right);
+		System.out.println("leftMaster output percent " + leftMaster.getMotorOutputPercent());
+		System.out.println("rightMaster output percent " + rightMaster.getMotorOutputPercent());
+		leftDrive1.set(leftMaster.getMotorOutputPercent());
+		leftDrive2.set(leftMaster.getMotorOutputPercent());
+		rightDrive1.set(rightMaster.getMotorOutputPercent());
+		rightDrive2.set(rightMaster.getMotorOutputPercent());
+	}
+	public void individualMotorDriveRPM(double left, double right, double MaxRPM) {
+		leftMaster.set(ControlMode.Velocity, left * MaxRPM);
+		rightMaster.set(ControlMode.Velocity, right * MaxRPM);
+		System.out.println("leftMaster output percent " + leftMaster.getMotorOutputPercent());
+		System.out.println("rightMaster output percent " + rightMaster.getMotorOutputPercent());
+		//make a RPM drive for Victors
+		/*leftDrive1.set(leftMaster.getMotorOutputPercent());
+		leftDrive2.set(leftMaster.getMotorOutputPercent());
+		rightDrive1.set(rightMaster.getMotorOutputPercent());
+		rightDrive2.set(rightMaster.getMotorOutputPercent());*/
 	}
 
     // Put methods for controlling this subsystem
